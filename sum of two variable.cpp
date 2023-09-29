cc09925c91dc60bf1bf007a2c49973e45bbe7130
ynotaman <146350867+ynotaman@users.noreply.github.com>
2023-09-29 23:31:03 +0530

    Add files via upload

---
+++ sum of two variable.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+    int a;
+    float b;
+     
+     printf ("enter a number a\n");
+     scanf ("%d",&a);
+     printf ("enter a number b\n");
+     scanf ("%f",&b);
+     printf ("the sum is %f\n",a+b);
+    return 0;
+     
+}
+
